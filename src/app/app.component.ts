67149d32e9840407bde753de7d3e225f74639af3
Munyiwamwangi <josseymunyi@gmail.com>
2019-07-12 12:33:19 +0300

    pipe checks the calories level and outputs the necessary meals with either high or low calories depending on user selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Meal } from './meal';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,10 +10,10 @@ export class DataService {
     { Name: 'Ugali', Desc: 'with veggies and beef stew', Calories: 200 },
     { Name: 'Pilau', Desc: 'with veggies and chicken, fried', Calories: 700 },
     { Name: 'Mukimo', Desc: 'with veggies and chicken soup', Calories: 500 },
-    { Name: 'Chips', Desc: 'with veggies and chicken breast', Calories: 348 }
+    { Name: 'Mashed Potatoes', Desc: 'with veggies and chicken breast', Calories: 348 }
   ];
   addMeal() {
-    return 'meal add succesfull';
+    return 'meal added';
   }
   GetMeals() {
     return this.meals;
